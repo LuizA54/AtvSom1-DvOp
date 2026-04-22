8bef2fb5218dc63bf8cca10033d5e00a98ae8ba2
Luiz Antônio <marlenelulu743@gmail.com>
2026-04-22 20:57:56 -0300

    Create validarLogin.test.js

---
+++ test/validarLogin.test.js
@@ -0,0 +1,17 @@
+const validarLogin = require('../src/validarLogin.js');
+
+test('deve retornar true para dados válidos', () => {
+    expect(validarLogin('teste@email.com', '123456')).toBe(true);
+});
+
+test('deve retornar false para email inválido', () => {
+    expect(validarLogin('testeemail.com', '123456')).toBe(false);
+});
+
+test('deve retornar false para senha curta', () => {
+    expect(validarLogin('teste@email.com', '123')).toBe(false);
+});
+
+test('deve retornar false para campos vazios', () => {
+    expect(validarLogin('', '')).toBe(false);
+});
